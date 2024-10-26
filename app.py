2507fc08a69d323aa3534c8a680fba510087e910
deveucatur <117214850+deveucatur@users.noreply.github.com>
2024-10-26 19:07:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,4 @@ st.set_page_config(page_title="Gestão de Adolescentes", layout="wide")
 
 st.title("Sistema de Gestão para Grupo de Adolescentes")
 
-st.sidebar.title("Navegação")
-page = st.sidebar.selectbox("Selecione a página", ["Cadastro", "Presença", "Aniversariantes", "Relatórios"])
 
-if page == "Cadastro":
-    import pages.cadastro
-elif page == "Presença":
-    import pages.presenca
-elif page == "Aniversariantes":
-    import pages.aniversariantes
-elif page == "Relatórios":
-    import pages.relatorios
